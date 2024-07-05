24a443a7cec427ead5772dd4479375a1a21dac38
LesegoLSG <lesegomhlongo78@gmail.com>
2024-07-05 02:48:39 -0500

    Included a form and prevented default

--- myChatApplication/src/Pages/ChatPage.jsx
+++ myChatApplication/src/Pages/ChatPage.jsx
@@ -12,14 +12,15 @@ import {
 } from "firebase/firestore";
 import { useCollection } from "react-firebase-hooks/firestore";
 import FlipMove from "react-flip-move";
-// import { data } from "autoprefixer";
+import { FiSend } from "react-icons/fi";
 
 const ChatPage = () => {
   const [input, setInput] = useState("");
   const [user] = useAuthState(auth);
   const lastMessage = useRef(null);
 
-  const sendMessage = () => {
+  const sendMessage = (e) => {
+    e.preventDefault();
     addDoc(collection(db, "chats"), {
       sender: user?.displayName,
       message: input,
@@ -47,7 +48,7 @@ const ChatPage = () => {
       <Header />
 
       {/* Body for chatpage */}
-      <div className="max-w-2xl mx-auto mt-2 bg-gray-200 shadow-lg rounded-lg">
+      <div className="max-w-2xl min-h-screen mx-auto mt-2 bg-gray-200 shadow-lg rounded-lg">
         {/* messages */}
         <div className="p-5 ">
           <FlipMove>
@@ -67,24 +68,29 @@ const ChatPage = () => {
           <div ref={lastMessage} className="mb-12" />
         </div>
         {/* Input */}
-        <div className="fixed  bottom-2  w-96 flex justify-between items-center space-x-2 ">
+        <form
+          className="fixed  bottom-2  w-96 flex justify-between items-center space-x-2 pr-8 pl-5 "
+          onSubmit={sendMessage}
+        >
           {/* <div className="w-full bg-blue-600"> */}
           <input
             type="text"
             placeholder="Send Me A Text"
             value={input}
             onChange={(e) => setInput(e.target.value)}
-            className="flex-1 outline-none bg-gray-300 p-4 rounded-lg"
+            className="flex-1 outline-none bg-gray-300 p-4 rounded-lg h-10"
           />
           <button
+            type="submit"
             disabled={!input}
-            className="bg-blue-200  px-6 py-4 rounded-lg font-bold disabled:bg-gray-200 disabled:cursor-not-allowed"
-            onClick={sendMessage}
+            className="bg-green-400 h-10 flex justify-center items-center  px-6 py-4 rounded-lg font-bold
+             disabled:bg-gray-300 disabled:cursor-not-allowed transform transition duration-200 hover:scale-110"
+            // onClick={sendMessage}
           >
-            Send
+            <FiSend />
           </button>
           {/* </div> */}
-        </div>
+        </form>
       </div>
     </section>
   );
